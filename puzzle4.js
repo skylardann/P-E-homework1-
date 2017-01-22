a319539893e7684210ae47fa0d10cdf70fd21003
skylardann <skylardann@cca.edu>
2017-01-22 15:13:18 -0800

    Create puzzle4.js

---
+++ puzzle4.js
@@ -0,0 +1,23 @@
+
+down();
+down();
+
+down();
+down();
+right();
+var c=getColor();
+down();
+setColor(c);
+
+
+right();
+right();
+up();
+right();
+var c=getColor();
+down();
+setColor(c);
+
+
+right();
+right();
