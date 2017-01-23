889f8fb4f2b81ef968baca0e78d978b1e363b8be
skylardann <skylardann@cca.edu>
2017-01-22 16:02:01 -0800

    Create level5.js

---
+++ level5.js
@@ -0,0 +1,29 @@
+
+right();
+right();
+
+
+if (getColor()=="blue") {
+  down();
+} else { 
+ up();
+
+}
+right();
+right();
+
+if (getColor()=="red") {
+  up();
+} else {
+  down();
+}  
+right();
+right();
+
+if (getColor()=="blue") {
+  down();
+} else { 
+ up();
+}
+right();
+right();
