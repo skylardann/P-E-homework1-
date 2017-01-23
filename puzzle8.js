9f5edb61b11b6aaa9c0fbf94916b3b09150d31f6
skylardann <skylardann@cca.edu>
2017-01-22 16:10:15 -0800

    Create puzzle8.js

---
+++ puzzle8.js
@@ -0,0 +1,59 @@
+var steps = 0;
+while (steps < 8) {
+  down();
+  steps = steps + 1;
+}
+
+var steps = 0;
+while (steps < 2) {
+  right();
+  steps = steps + 1;
+}
+
+var steps = 0;
+while (steps < 7) {
+  up();
+  steps = steps + 1;
+}
+
+var steps = 0;
+while (steps < 2) {
+  right();
+  steps = steps + 1;
+}
+
+var steps = 0;
+while (steps < 7) {
+  down();
+  steps = steps + 1;
+}
+
+var steps = 0;
+while (steps < 2) {
+  right();
+  steps = steps + 1;
+}
+
+var steps = 0;
+while (steps < 7) {
+  up();
+  steps = steps + 1;
+}
+
+var steps = 0;
+while (steps < 2) {
+  right();
+  steps = steps + 1;
+}
+var steps = 0;
+while (steps < 7) {
+  down();
+  steps = steps + 1;
+}
+var steps = 0;
+while (steps < 1) {
+  right();
+  steps = steps + 1;
+}
+
+
